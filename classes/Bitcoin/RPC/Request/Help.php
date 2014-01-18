69f2c86f92cb7488fc8a75a43b90916105733223
Dennis Ruhe <dennis.r.ruhe@gmail.com>
2014-01-18 17:19:28 +0100

    Added new requests

---
+++ classes/Bitcoin/RPC/Request/Help.php
@@ -0,0 +1,3 @@
+<?php defined('SYSPATH') or die('No direct script access');
+
+class Bitcoin_RPC_Request_Help extends Kohana_Bitcoin_RPC_Request_Help {}
--- classes/Kohana/Bitcoin/RPC/Request/Help.php
+++ classes/Kohana/Bitcoin/RPC/Request/Help.php
@@ -1,6 +1,6 @@
 <?php defined('SYSPATH') or die('No direct script access');
 
-class Kohana_Bitcoin_RPC_Request_Getinfo extends Bitcoin_RPC_Request {
+class Kohana_Bitcoin_RPC_Request_Help extends Bitcoin_RPC_Request {
 
 	protected $_is_cacheable = TRUE;
 }

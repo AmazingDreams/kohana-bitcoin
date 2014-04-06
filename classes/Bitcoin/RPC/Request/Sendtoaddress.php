571d7989a5690ecbb87cce3da688e6ee7979b8f4
Dennis Ruhe <dennis@amazingsystems.nl>
2014-04-06 13:34:24 +0200

    Added sendtoaddress request

--- classes/Bitcoin/RPC/Request/Sendtoaddress.php
+++ classes/Bitcoin/RPC/Request/Sendtoaddress.php
@@ -1,11 +1,11 @@
 <?php defined('SYSPATH') or die('No direct script access');
 
 /**
- * Class Bitcoin RPC Request Sendmany
+ * Class Bitcoin RPC Request Sendtoaddress
  *
  * @package    Kohana/Bitcoin
  * @category   Helper
  * @author     Dennis Ruhe
  * @copyright  (c) 2013 Dennis Ruhe
  */
-class Bitcoin_RPC_Request_Sendmany extends Kohana_Bitcoin_RPC_Request_Sendmany {}
+class Bitcoin_RPC_Request_Sendtoaddress extends Kohana_Bitcoin_RPC_Request_Sendtoaddress {}
